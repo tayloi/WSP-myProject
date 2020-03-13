3bd86d4f79803f5db7e5a1ae7ba96ba41c4d1276
tayloi <taylor1201am@gmail.com>
2020-03-12 23:02:49 -0400

    Serving up the client code from the server

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const exerciseCategories = require('./controllers/fitnessTracker');
+const path = require('path');
 
 const app = express();
 const port = 3000;
@@ -7,8 +8,15 @@ const port = 3000;
 app
     .use(express.json())
     .use(express.urlencoded({ extended: true }))
+    .use(express.static( __dirname + '/../client/dist'))
     .get('/', (req, res) => res.send('Welcome to the fitness tracker!')) // <-- lamda expression used here and below
 //when msg sent w/verb 'get' and '/', call this funciton
-    .use('/fitnessTracker', fitnessTrackerController);
+    .use('/fitnessTracker', fitnessTrackerController)
+    
+    .use((req, res) => {
+        const homePath = path.join( __dirname , '/../client/dist/index.html');
+        console.log(homePath);
+        res.sendFile(homePath)
+    });
 
 app.listen(port, () => console.log(`Listening at http://localhost:${port}`));
