48cc1d098eb0b32743c4978dbf6a3c40a8d71157
tayloi <taylor1201am@gmail.com>
2020-05-05 17:16:53 -0400

    Added more users

--- client/src/models/Users.js
+++ client/src/models/Users.js
@@ -3,7 +3,10 @@
  */
 const Users = [
     { Name: 'Imani', Password: '2020', Email: 'taylori1@newpaltz.edu', isAdmin: true },
-    { Name: 'User1', Password: '1234', Email: 'user1@newpaltz.edu', isAdmin: false }
+    { Name: 'User1', Password: '1234', Email: 'user1@newpaltz.edu', isAdmin: false },
+    { Name: 'Adam', Password: '2222', Email: 'adam@email.com', userId: 2, isAdmin: false },
+    { Name: 'Branda', Password: '3333', Email: 'branda@email.com', userId: 3, isAdmin: false },
+    { Name: 'Carmen', Password: '4444', Email: 'carmen@email.com', userId: 4, isAdmin: false }
 ];
 
 export let CurrentUser = null;
