d984d8f398e92299ce8143861950a2dfba75ef1f
tayloi <taylor1201am@gmail.com>
2020-05-05 18:33:54 -0400

    Updated navbar and About page

--- client/src/components/Nav.vue
+++ client/src/components/Nav.vue
@@ -1,38 +1,42 @@
 <template>
-<nav class="navbar" role="navigation" aria-label="main navigation">
-  <div class="navbar-brand">
-    <a class="navbar-item" href="/">
-      <img alt="Fitness tracker logo" width="112" height="28">
-    </a>
+  <nav role="navigation" aria-label="main navigation" style="padding-bottom: 2%">
+    <div class="navbar has-background-grey-lighter">
+      <div class="navbar-brand">
+        <a class="navbar-item has-background-primary" href="/">
+          <i class="fas fa-running"></i>
+          <span class="" style="padding: 0 5px 0 5px">FitnessTracker</span> 
+          <i class="far fa-calendar-alt"></i>
+        </a>
 
-    <a :class="{ 'is-active':isOpen }" @click="isOpen = !isOpen" role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" >
-      <span aria-hidden="true"></span>
-      <span aria-hidden="true"></span>
-      <span aria-hidden="true"></span>
-    </a>
-  </div>
+        <a :class="{ 'is-active':isOpen }" @click="isOpen = !isOpen" role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" >
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
 
-  <div :class="{ 'is-active':isOpen }" class="navbar-menu">
-    <div class="navbar-start">
-      <router-link to="/" class="navbar-item">Home</router-link>
-      <router-link to="/about" class="navbar-item">About</router-link>
-      <router-link to="/fitnessTracker" class="navbar-item">Fitness Tracker</router-link>
-    </div>
+      <div :class="{ 'is-active':isOpen }" class="navbar-menu">
+        <div class="navbar-start">
+          <router-link to="/" class="navbar-item">Home</router-link>
+          <router-link to="/about" class="navbar-item">About</router-link>
+          <router-link to="/fitnessTracker" class="navbar-item">My Fitness</router-link>
+        </div>
 
-    <div class="navbar-end">
-      <div class="navbar-item">
-        <div class="buttons">
-          <a class="button is-primary">
-            <strong>Sign up</strong>
-          </a>
-          <a class="button is-light" href="/login">
-            Log in
-          </a>
+        <div class="navbar-end">
+          <div class="navbar-item">
+            <div class="buttons">
+              <a class="button is-primary">
+                <strong>Sign up</strong>
+              </a>
+              <a class="button is-light" href="/login">
+                Log in
+              </a>
+            </div>
+          </div>
         </div>
       </div>
     </div>
-  </div>
-</nav>
+  </nav>
 </template>
 
 <script>
--- client/src/views/About.vue
+++ client/src/views/About.vue
@@ -2,8 +2,9 @@
   <section class="container">
     <div class="hero">
         <h1 class="title">
-          This is a fitness tracker. Use this site to track your exercises and meals each day!
+          About FitnessTracker
         </h1>
+        <h5 class="title is-5"> With FitnessTracker, you can track your exercises and meals each day! You can even add friends and see their progress too!</h5>
     </div>
   </section>
 </template>
