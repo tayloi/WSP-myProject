77cfaf0986ae70f5a2da70d22a314ee084c104d2
tayloi <taylor1201am@gmail.com>
2020-05-08 00:18:33 -0400

    addCategory server side

--- server/controllers/fitnessTracker.js
+++ server/controllers/fitnessTracker.js
@@ -14,8 +14,8 @@ router
         addExercise: fitnessTracker.addExercise, addMeal: fitnessTracker.addMeal, addFriend: fitnessTracker.addFriend
      }) )
     .get('/exerciseCategories', (req, res) => res.send(exerciseCategories) )
-    .post('exerciseCategories', (req, res) => {
-        exerciseCategories.add(req.body.text);
+    .post('/addCategory', (req, res) => {
+        exerciseCategories.add(req.body.category);
         res.send(exerciseCategories.list[exerciseCategories.lenth-1]);
     });
 
